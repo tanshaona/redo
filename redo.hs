63ecb08f668316f93d2bf228b8b08839dfa988fa
zhuanxu <tanshaona@126.com>
2014-01-31 13:07:13 +0800

    add middle file

--- redo.hs
+++ redo.hs
@@ -1,6 +1,11 @@
 -- file: redo.hs
+import System.Exit (ExitCode(..))
 import System.Environment (getArgs)
+import System.IO (hPutStrLn, stderr)
+import System.Directory (removeFile, renameFile)
 import System.Process (createProcess, waitForProcess, shell)
+
+
 main :: IO ()
 main = do
     args <- getArgs
@@ -8,6 +13,10 @@ main = do
 
 redo :: String -> IO ()
 redo target = do
-    (_,_,_,ph) <- createProcess $ shell $ "sh " ++ target ++ ".do"
-    _ <- waitForProcess ph
-    return ()
+    let tmp = target ++ "----redoing"
+    (_,_,_,ph) <- createProcess $ shell $ "sh " ++ target ++ ".do - - " ++ tmp -- ++ " > " ++ tmp
+    exit <- waitForProcess ph
+    case exit of
+        ExitSuccess -> do renameFile tmp target
+        ExitFailure code -> do hPutStrLn stderr $ "Redo script exited with non-zero exit code: " ++ show code
+                               removeFile tmp
